660f3867565481a3d46c4de24c0e00b5793d9d5f
Juan Sergio Zeballos Perez <jzeballosp@unsa.edu.pe>
2024-06-08 02:22:41 -0500

    Pequeño error solucionado

--- listaContactos/urls.py
+++ listaContactos/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from inicio.views import myHomeView
+from inicio.views import anotherView
 
 urlpatterns = [
     path('', myHomeView, name='Pagina de inicio'),
