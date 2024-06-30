37dbc283f005871e170ec28f645d78a5a42b15c0
Juan Sergio Zeballos Perez <jzeballosp@unsa.edu.pe>
2024-06-29 21:04:17 -0500

    renderizacion de una persona

--- src/listaContactos/listaContactos/urls.py
+++ src/listaContactos/listaContactos/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from inicio.views import myHomeView
 from inicio.views import anotherView
+from personas.views import personaTestView
 
 urlpatterns = [
-    path('', myHomeView, name='Pagina de Inicio'),
-    path('another/', anotherView),
+    path('', myHomeView, name='home'),
+    path('another/', anotherView, name='otro'),
+    path('persona/', personaTestView, name='otro'),
     path('admin/', admin.site.urls),
 ]
